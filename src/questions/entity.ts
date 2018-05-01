1ce6ff98c0765a7ae8759f243c6f78950515dcd6
John <john_ekelmans@msn.com>
2018-05-01 17:19:30 +0200

    questions

--- src/questions/entity.ts
+++ src/questions/entity.ts
@@ -1,8 +1,34 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
 
 
+@Entity()
+export class Question extends BaseEntity {
 
+  @PrimaryGeneratedColumn()
+  id?: number
 
+  @Column('integer', {})
+  questionnumber: number
 
-export default class QustionController {
-  @HttpCode(201)
+  @Column('integer', {})
+  quizId: number
+
+  @Column('text', {})
+  title: String
+
+  @Column('text', {})
+  option1: String
+
+  @Column('text', {})
+  option2: String
+
+  @Column('text', {})
+  option3: String
+
+  @Column('text', {})
+  option4: String
+
+  @Column('number', {})
+  correctAnswer: number
+
 }
